2b0f21b4ebe68bbc53cc95d3a5c6c4888fc15b4e
AnandaRFebriyana <e41222236@student.polije.ac.id>
2024-02-21 20:47:01 +0700

    add Tugas Minggu3

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biodata Diri</title>
+</head>
+<body>
+    <p>Ananda Rizky Febriyana</p>
+    <p>Jurusan Teknologi Informasi</p>
+    <p>Prodi Teknik Informatika</p>
+    <p>Golongan E</p>
+</body>
+</html>
